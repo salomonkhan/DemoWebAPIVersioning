10bb5dfc71e1db01f91baf44b44d4f977e4dc244
Khan,Salman <salman.khan@freedompay.com>
2018-08-16 13:13:30 +0500

    Sample code for web api versioning

---
+++ WebAPIVersioning/Controllers/StudentsV1Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using WebAPIVersioning.DTOs;
+
+namespace WebAPIVersioning.Controllers
+{
+    public class StudentsV1Controller : ApiController
+    {
+        List<StudentV1> students = new List<StudentV1>()
+        {
+            new StudentV1() { Id = 1, Name = "Tom"},
+            new StudentV1() { Id = 2, Name = "Sam"},
+            new StudentV1() { Id = 3, Name = "John"},
+        };
+        
+        public IEnumerable<StudentV1> Get()
+        {
+            return students;
+        }
+        public StudentV1 Get(int id)
+        {
+            return students.FirstOrDefault(s => s.Id == id);
+        }
+    }
+}
---
+++ WebAPIVersioning/Custom/CustomControllerSelector.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Dispatcher;
+
+namespace WebAPIVersioning.Custom
+{
+    public class CustomControllerSelector : DefaultHttpControllerSelector
+    {
+        private HttpConfiguration _config;
+        public CustomControllerSelector(HttpConfiguration config) : base(config)
+        {
+            _config = config;
+        }
+
+        public override HttpControllerDescriptor
+            SelectController(HttpRequestMessage request)
+        {
+            // Get all the available Web API controllers
+            var controllers = GetControllerMapping();
+            // Get the controller name and parameter values from the request URI
+            var routeData = request.GetRouteData();
+
+            // Get the controller name from route data.
+            // The name of the controller in our case is "Students"
+            var controllerName = routeData.Values["controller"].ToString();
+
+            // Default version number to 1
+            string versionNumber = "1";
+            //var versionQueryString = HttpUtility.ParseQueryString(request.RequestUri.Query);
+            //if (versionQueryString["v"] != null)
+            //{
+            //    versionNumber = versionQueryString["v"];
+            //}
+            string customHeader = "X-StudentService-Version";
+            if (request.Headers.Contains(customHeader))
+            {
+                versionNumber = request.Headers.GetValues(customHeader).FirstOrDefault();
+            }
+
+            if (versionNumber == "1")
+            {
+                // if version number is 1, then append V1 to the controller name.
+                // So at this point the, controller name will become StudentsV1
+                controllerName = controllerName + "V1";
+            }
+            else
+            {
+                // if version number is 2, then append V2 to the controller name.
+                // So at this point the, controller name will become StudentsV2
+                controllerName = controllerName + "V2";
+            }
+
+            HttpControllerDescriptor controllerDescriptor;
+            if (controllers.TryGetValue(controllerName, out controllerDescriptor))
+            {
+                return controllerDescriptor;
+            }
+
+            return null;
+        }
+    }
+}
